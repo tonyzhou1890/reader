60eadde6ffe1b21c84880f030caf355ba2f0de8e
tony <1332988826@qq.com>
2019-04-03 13:55:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# reader
+
+> 电子书阅读器--只支持txt格式
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import '@/styles/index.scss' // global css
+import './icons' // icon
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
