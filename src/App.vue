4429869409e96f4e01cbfffb8141cdb4f783bde1
Tony <1332988826@qq.com>
2019-04-03 23:30:50 +0800

    show text for url

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,82 @@
 <template>
   <div id="app">
-    reader
+    <tu-book
+      :text="message"
+      :width="width"
+      :height="height"
+      :percent="defaultPercent"
+      :single="single"
+      @pageChange="pageChange"
+    />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      width: null,
+      height: null,
+      message: 'reader',
+      total: null,
+      page: null,
+      percent: null,
+      defaultPercent: null
+    }
+  },
+  computed: {
+    single() {
+      let temp = (this.width && this.height) || false
+      temp = this.width < this.height
+      return temp
+    }
+  },
+  created() {
+    this.settingSize()
+    this.watchClientSize()
+    this.resolveUrl()
+  },
+  methods: {
+    watchClientSize() {
+      window.addEventListener('resize', this._.debounce(this.settingSize, 1000))
+    },
+    settingSize() {
+      const s = window.getComputedStyle(document.body)
+      this.defaultPercent = this.percent
+      this.width = Number(s.width.split('px')[0])
+      this.height = Number(s.height.split('px')[0])
+    },
+    pageChange(data) {
+      this.total = data.total
+      this.page = data.page
+      this.percent = data.percent
+    },
+    resolveUrl() {
+      const temp = window.location.search.slice(1).split('&')
+      temp.map((item, index) => {
+        temp[index] = item.split('=')
+      })
+      // 是否通过address指定文本地址
+      const address = temp.filter(item => { return item[0] === 'address' })
+      if (address.length) {
+        this.getAddressTxt(address[0][1])
+      }
+    },
+    getAddressTxt(url) {
+      axios.get(url)
+        .then(res => {
+          this.message = res.data
+        })
+        .catch(e => {
+          this.$message({
+            message: '地址错误',
+            type: 'error'
+          })
+        })
+    }
+  }
 }
 </script>
 
@@ -15,7 +85,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,27 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import '@/styles/index.scss' // global css
 import './icons' // icon
 
+import 'element-ui/lib/theme-chalk/index.css'
+import { Message, MessageBox, Loading } from 'element-ui'
+
+import ui from 'tiny-utils.js/UI'
+
+import debounce from 'lodash/debounce'
+
+Vue.use(Loading.directive)
+
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$message = Message
+
+Object.keys(ui).map(item => {
+  Vue.component(item, ui[item])
+})
+
+Vue.prototype._ = {
+  debounce
+}
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
